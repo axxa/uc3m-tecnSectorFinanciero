3decf0ee8f38de36628a2910f13222969483ea58
Alvaro <asan123456@gmail.com>
2019-10-31 10:00:48 +0100

    feat: first part working

--- Lesson3/src/main/java/com/cnebrera/uc3/tech/lesson3/app/Publisher.java
+++ Lesson3/src/main/java/com/cnebrera/uc3/tech/lesson3/app/Publisher.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2015 Kaazing Corporation
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.cnebrera.uc3.tech.lesson3.app;
 
 import java.util.concurrent.TimeUnit;
@@ -24,9 +9,6 @@ import org.agrona.concurrent.UnsafeBuffer;
 import io.aeron.Aeron;
 import io.aeron.Publication;
 
-/**
- * A very simple Aeron publisher application which publishes a fixed size message on a fixed channel and stream.
- */
 public class Publisher
 {
     public static void main(final String[] args) throws Exception
@@ -59,7 +41,7 @@ public class Publisher
             buffer.putBytes(0, messageBytes);
 
             // Wait for 5 seconds to connect to a subscriber
-            /*
+            
             final long deadlineNs = System.nanoTime() + TimeUnit.SECONDS.toNanos(5);
             while (!publication.isConnected())
             {
@@ -71,7 +53,7 @@ public class Publisher
 
                 Thread.sleep(1);
             }
-            */
+            
             // Try to publish the buffer. 'offer' is a non-blocking call.
             // If it returns less than 0, the message was not sent, and the offer should be retried.
             final long result = publication.offer(buffer, 0, messageBytes.length);
--- Lesson3/src/main/java/com/cnebrera/uc3/tech/lesson3/app/Subscriber.java
+++ Lesson3/src/main/java/com/cnebrera/uc3/tech/lesson3/app/Subscriber.java
@@ -1,40 +1,14 @@
-/*
- * Copyright 2015 Kaazing Corporation
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.cnebrera.uc3.tech.lesson3.app;
 
-import org.agrona.DirectBuffer;
 import org.agrona.concurrent.BusySpinIdleStrategy;
 import org.agrona.concurrent.IdleStrategy;
 
 import io.aeron.Aeron;
-import io.aeron.FragmentAssembler;
 import io.aeron.Subscription;
 import io.aeron.logbuffer.FragmentHandler;
-import io.aeron.logbuffer.Header;
 
-/**
- * A very simple Aeron subscriber application which can receive small non-fragmented messages
- * on a fixed channel and stream ID. The DataHandler method 'printStringMessage' is called when data
- * is received. This application doesn't handle large fragmented messages. For an example of
- * fragmented message reception, see {@link MultipleSubscribersWithFragmentAssembly}.
- */
 public class Subscriber
 {
-
-    //final FragmentHandler fragmentHandler = new FragmentAssembler(this::onFragmentReceived);
     
     public static void main(final String[] args)
     {
@@ -50,7 +24,6 @@ public class Subscriber
         
         System.out.println("Subscribing to " + channel + " on stream id " + streamId);
         final Aeron.Context ctx = new Aeron.Context();
-        
         
 
         final FragmentHandler fragmentHandler =
@@ -69,39 +42,27 @@ public class Subscriber
             };
 
     
+        Aeron connection = Aeron.connect(ctx);
+        Subscription subscription = connection.addSubscription(channel, streamId);
 
-        
-        try (Aeron aeron = Aeron.connect(ctx);
-            Subscription subscription = aeron.addSubscription(channel, streamId))
+        Thread pollerThread = new Thread(() ->
         {
             
-            Thread pollerThread = new Thread(() ->
+            IdleStrategy idleStrategy = new BusySpinIdleStrategy();
+            while (true)
             {
-                
-                final IdleStrategy idleStrategy = new BusySpinIdleStrategy();
-                while (true)
-                {
-                    final int result = subscription.poll(fragmentHandler, 1);
-                    idleStrategy.idle(result);
-                
-                }
-                
-            });
-
-            pollerThread.start();
+                int result = subscription.poll(fragmentHandler, fragmentLimitCount);
+                idleStrategy.idle(result);
             
-        }
+            }
+            
+        });
+
+        pollerThread.start();
+            
+        
     }
 
-    
-    void onFragmentReceived(DirectBuffer buffer, int offset, int length, Header header)
-    {
-        final byte[] data = new byte[length];
-        buffer.getBytes(offset, data);
-        final String rcvString = new String(data);
-        System.out.println(rcvString);
-    }
-
-
+ 
 
 }
