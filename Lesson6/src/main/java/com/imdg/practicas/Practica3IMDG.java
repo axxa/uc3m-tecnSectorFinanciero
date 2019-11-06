338b598655707d24a614790ce7cd1e375d26f35d
Alvaro <asan123456@gmail.com>
2019-11-06 12:35:41 +0100

    feat: part3 working

--- Lesson6/src/main/java/com/imdg/practicas/Practica3IMDG.java
+++ Lesson6/src/main/java/com/imdg/practicas/Practica3IMDG.java
@@ -2,15 +2,16 @@ package com.imdg.practicas;
 
 import com.hazelcast.client.HazelcastClient;
 import com.hazelcast.client.config.ClientConfig;
-import com.hazelcast.config.Config;
 import com.hazelcast.core.HazelcastInstance;
+import com.imdg.datagrid.DataGridNode;
+import com.imdg.pojos.Person;
 
 import java.util.ArrayList;
 
 public class Practica3IMDG {
 
     public static void main(String[] args) {
-	    // Instanciar hazelcast Cliente y crear una cache
+        // Instanciar hazelcast Cliente y crear una cache
         ClientConfig config = new ClientConfig();
         ArrayList<String> ips=new ArrayList();
         ips.add("127.0.0.1");
@@ -21,15 +22,13 @@ public class Practica3IMDG {
 
 
 
-
+        
         //Vuestro código va aqui
-
-
-
-
-
-
-
+        DataGridNode node = new DataGridNode();
+        node.printCache();
+        Person p = new Person("Alvaro", 28005, "", "");
+        node.addToCache(p);
+        node.printCache();
 
 
 
