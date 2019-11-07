1c0cbca1a7a99f1968137e439d18c42f941f3ca1
Alvaro <asan123456@gmail.com>
2019-11-07 11:23:15 +0100

    feat: part1 working

--- Lesson7/src/main/java/com/imdg/listeners/VolumeListener.java
+++ Lesson7/src/main/java/com/imdg/listeners/VolumeListener.java
@@ -9,6 +9,7 @@ import com.hazelcast.map.listener.MapListener;
 import com.imdg.pojos.MarketOrder;
 
 import java.io.Serializable;
+import java.util.HashMap;
 
 /**
  * Created by Sobremesa on 31/10/2016.
@@ -16,12 +17,14 @@ import java.io.Serializable;
 public class VolumeListener
             implements EntryAddedListener<String, MarketOrder>,
         EntryUpdatedListener<String, MarketOrder>, Serializable {
-
+         
     private String instrumentoAControlar;
     private int volumenAcumulado=0;
-
+    private HashMap<String, MarketOrder> orderMap;
+    
     public VolumeListener(String instrument) {
         this.instrumentoAControlar=instrument;
+        this.orderMap = new HashMap<String, MarketOrder>();
     }
 
     /**
@@ -30,9 +33,15 @@ public class VolumeListener
      */
     @Override
     public void entryAdded(EntryEvent<String, MarketOrder> entryEvent) {
+        MarketOrder order = entryEvent.getValue();
+        
+        if (order.getInstrument().equals(this.instrumentoAControlar)) {
+            this.volumenAcumulado += order.getVolume();
+            this.orderMap.put(entryEvent.getKey(), order);
+        }
 
+        verifyOrderVolumn();
     }
-
     /**
      * Escuchar entradas que se añaden, restar valor antiguo y
      * sumar el nuevo al volumen/imprimir alerta si llegamos a 30000
@@ -40,6 +49,21 @@ public class VolumeListener
      */
     @Override
     public void entryUpdated(EntryEvent<String, MarketOrder> entryEvent) {
+        MarketOrder currentOrder = this.orderMap.get(entryEvent.getKey());
+        MarketOrder order = entryEvent.getValue();
 
+        if (order.getInstrument().equals(this.instrumentoAControlar)) {
+            this.volumenAcumulado += (order.getVolume() - currentOrder.getVolume());
+            this.orderMap.put(entryEvent.getKey(), order);
+        }
+
+        verifyOrderVolumn();
+    }
+
+    private void verifyOrderVolumn(){
+        if (this.volumenAcumulado > 30000){
+            System.out.println("\nAlerta: 30000 unidades acumuladas\n");
+            this.volumenAcumulado = 0;
+        }
     }
 }
