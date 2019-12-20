f86d75bdb321ca563d3b2bccca776a19707ca930
Alvaro <asan123456@gmail.com>
2019-12-20 19:12:44 +0100

    feat: microservice example working

--- Lesson12/com.practica12.cloud.microservice_example/src/main/java/com/practica12/cloud/microservice_example/Application.java
+++ Lesson12/com.practica12.cloud.microservice_example/src/main/java/com/practica12/cloud/microservice_example/Application.java
@@ -1,29 +1,75 @@
 package com.practica12.cloud.microservice_example;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
 @SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
 @RestController
+@EnableConfigurationProperties(MyProps.class)
+@RefreshScope
 public class Application
 {
-	@RequestMapping("/")
-	/**
-	 * Controller basico. Muestra un mensaje
-	 */
-	public String home(){
-		return "Hello World!";
+	@Autowired
+	private Service service;
+	@Autowired
+	private MyProps props;
+	@Value("${myprops.alias:alias!}")
+	String alias;
+	@Bean
+	@RefreshScope
+	public Service service()
+	{
+		return new Service(props.getName());
 	}
-	/**
-	 * Clase principal que arranca la aplicacion JAVA - Spring boot. De estar siempre en los
-	servicios y ha de ser la primera de la jerarquia de
-	* paquetes
-	* @param args no tiene parametros
-	*/
-	public static void main(String[] args){
+
+	@RequestMapping("/")
+	public String home() {
+		return "Hello " + service.getName() + "!" + " with alias " + alias;
+	}
+
+	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 }
 
+@Data
+@RequiredArgsConstructor
+class Service {
+	private final String name;
+
+	Service(String name)
+	{
+		this.name = name;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+}
+
+
+@Data
+@ConfigurationProperties("myprops")
+class MyProps
+{
+ 	private String name = "World";
+
+	public String getName() {
+		return this.name;
+	}
+}
