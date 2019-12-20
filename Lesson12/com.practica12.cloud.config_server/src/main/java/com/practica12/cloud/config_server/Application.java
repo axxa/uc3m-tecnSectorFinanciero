3b88b6d425aded775527067820250f2c9c790a7c
Alvaro <asan123456@gmail.com>
2019-12-20 22:08:35 +0100

    feat: configServer to be discovered by eureka

--- Lesson12/com.practica12.cloud.config_server/src/main/java/com/practica12/cloud/config_server/Application.java
+++ Lesson12/com.practica12.cloud.config_server/src/main/java/com/practica12/cloud/config_server/Application.java
@@ -3,11 +3,13 @@ package com.practica12.cloud.config_server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 @EnableConfigServer
 @Configuration
+@EnableEurekaClient
 public class Application {
 
 	public static void main(String[] args) {
