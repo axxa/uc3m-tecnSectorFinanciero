b2d3f4b8864ee2470257d1bd6a7dc7a631e15b48
Alvaro <asan123456@gmail.com>
2019-12-20 11:25:21 +0100

    feat: config server

---
+++ Lesson12/com.practica12.cloud.config_server/src/main/java/com/practica12/cloud/config_server/Application.java
@@ -0,0 +1,20 @@
+package com.practica12.cloud.config_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootApplication
+@EnableConfigServer
+@Configuration
+public class Application
+{
+ /**
+ * Metodo principal para arrancar el config server
+ * @param args
+ */
+	public static void main(String[] args){
+		SpringApplication.run(Application.class, args);
+	}
+}
