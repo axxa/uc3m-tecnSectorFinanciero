bf05d507a257b607c8e069f1ee6c7f4554803e0a
Alvaro <asan123456@gmail.com>
2019-12-21 11:22:45 +0100

    feat: hystrix dashboard working

---
+++ Lesson12/com.practica12.cloud.hystrix/src/main/java/com/practica12/cloud/hystrix/Application.java
@@ -0,0 +1,17 @@
+package com.practica12.cloud.hystrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableHystrixDashboard
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
